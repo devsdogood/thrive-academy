e7ca377b31e0d8540984671aa5d138f579f48dbd
jaethem8 <jaehyeok.choi@drake.edu>
2022-01-29 13:27:21 -0600

    add underline navbar

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,25 +1,34 @@
 import styles from "../styles/NavigationMenu/NavigationMenu.module.css";
 import { INavigationItem } from "@src/types/generated/contentful";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "/public/logoForHeader.jpg";
 
 const NavigationMenu: React.FC<{ menuItems: INavigationItem[] }> = ({
   menuItems,
 }) => (
   <>
-    <div className={styles.navigationMenu}>
-      {menuItems.map((item) => (
-        <Link
-          key={item.sys.id}
-          href={item.fields.page?.fields.slug || item.fields.externalUrl || "/"}
-          passHref
-        >
-          <div key={item.sys.id} className={styles.navigationLink}>
-            <span className="fw-bold p-3" style={{ color: "white" }}>
-              {item.fields.title}
-            </span>
-          </div>
-        </Link>
-      ))}
+    <div className={styles.headerWrapper}>
+      <div className={styles.headerContent}>
+        <div className={styles.imageWrapper}>
+          <Image src={logo}></Image>
+        </div>
+        <div className={styles.rightHeader}>
+          {menuItems.map((item) => (
+            <Link
+              key={item.sys.id}
+              href={
+                item.fields.page?.fields.slug || item.fields.externalUrl || "/"
+              }
+              passHref
+            >
+              <div key={item.sys.id} className={styles.navbars}>
+                <span className="fw-bold p-3">{item.fields.title}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   </>
 );
