625c80d561df296dc38764d496d99302f549e23b
jaethem8 <jaehyeok.choi@drake.edu>
2022-01-29 13:56:45 -0600

    add eventlistener

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -3,34 +3,61 @@ import { INavigationItem } from "@src/types/generated/contentful";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "/public/logoForHeader.jpg";
+import { useState, useEffect } from "react";
 
 const NavigationMenu: React.FC<{ menuItems: INavigationItem[] }> = ({
   menuItems,
-}) => (
-  <>
-    <div className={styles.headerWrapper}>
-      <div className={styles.headerContent}>
-        <div className={styles.imageWrapper}>
-          <Image src={logo}></Image>
-        </div>
-        <div className={styles.rightHeader}>
-          {menuItems.map((item) => (
-            <Link
-              key={item.sys.id}
-              href={
-                item.fields.page?.fields.slug || item.fields.externalUrl || "/"
-              }
-              passHref
-            >
-              <div key={item.sys.id} className={styles.navbars}>
-                <span className="fw-bold p-3">{item.fields.title}</span>
-              </div>
+}) => {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return (
+    <>
+      <div className={styles.headerWrapper}>
+        <div className={styles.headerContent}>
+          <div className={styles.imageWrapper}>
+            <Link href="/">
+              <Image src={logo} className={styles.logoImage}></Image>
             </Link>
-          ))}
+          </div>
+          <div className={styles.rightHeader}>
+            {menuItems.map((item) => (
+              <Link
+                key={item.sys.id}
+                href={
+                  item.fields.page?.fields.slug ||
+                  item.fields.externalUrl ||
+                  "/"
+                }
+                passHref
+              >
+                <div key={item.sys.id} className={styles.navbars}>
+                  <span className="fw-bold p-3">{item.fields.title}</span>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default NavigationMenu;
