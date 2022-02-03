bb1b2e7f3dd6cbd81c2bad40fed2fec37ae58556
Travis Sanderson <travissanderson@gmail.com>
2022-02-03 14:34:48 -0600

    Hook up basics of Site Settings

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { FaEnvelope, FaFacebook } from "react-icons/fa";
+import { ISettingsFields } from "@src/types/generated/contentful";
 
 type Props = {
+    settings: ISettingsFields
 };
 
-const Footer: React.FC<Props> = ({ children }) => {
+const Footer: React.FC<Props> = ({ settings, children }) => {
     return (
         <footer>
             <div
@@ -23,28 +25,31 @@ const Footer: React.FC<Props> = ({ children }) => {
                                         layout="fixed"
                                         width={175}
                                         height={101}
-                                        src="/thriveLogoFooter.png"
+                                        src={`https:${settings.logo.fields.file.url}`}
                                         alt="Thrive Academy"
                                     />
                                 </a>
                             </Link>
                             <ul className="list-unstyled">
                                 <li className="mb-2">West Des Moines, IA</li>
-                                <li className="mb-2">
-                                    <Link href="https://www.facebook.com/groups/1138474949915990/">
-                                        <a>
-                                            <FaFacebook /> Thrive on Facebook
-                                        </a>
-                                    </Link>
-
-                                </li>
-                                <li className="mb-2">
-                                    <Link href="mailto:thriveacademyia@gmail.com">
-                                        <a>
-                                            <FaEnvelope /> thriveacademyia@gmail.com
-                                        </a>
-                                    </Link>
-                                </li>
+                                {settings.facebook !== undefined && settings.facebook.length > 0 &&
+                                    <li className="mb-2">
+                                        <Link href={`${settings.facebook}`}>
+                                            <a>
+                                                <FaFacebook /> Thrive on Facebook
+                                            </a>
+                                        </Link>
+                                    </li>
+                                }
+                                {settings.email !== undefined && settings.email.length > 0 &&
+                                    <li className="mb-2">
+                                        <Link href={`mailto:${settings.email}`}>
+                                            <a>
+                                                <FaEnvelope /> {settings.email}
+                                            </a>
+                                        </Link>
+                                    </li>
+                                }
                             </ul>
                         </Col>
                         <Col lg={2} md={3} sm={4} className="mb-5 mb-md-0">
@@ -158,13 +163,24 @@ const Footer: React.FC<Props> = ({ children }) => {
                         </Col>
                         <Col sm={5} className="text-sm-right text-center">
                             <ul className="list-inline">
-                                <li className="list-inline-item">
-                                    <Link href="https://www.facebook.com/groups/1138474949915990/">
-                                        <a className="d-inline-block p-2">
-                                            <FaFacebook />
-                                        </a>
-                                    </Link>
-                                </li>
+                                {settings.facebook !== undefined && settings.facebook.length > 0 &&
+                                    <li className="list-inline-item">
+                                        <Link href={`${settings.facebook}`}>
+                                            <a className="d-inline-block p-2" title="Join us on Facebook">
+                                                <FaFacebook />
+                                            </a>
+                                        </Link>
+                                    </li>
+                                }
+                                {settings.email !== undefined && settings.email.length > 0 &&
+                                    <li className="list-inline-item">
+                                        <Link href={`mailto:${settings.email}`}>
+                                            <a className="d-inline-block p-2" title={`Email us at ${settings.email}`}>
+                                                <FaEnvelope />
+                                            </a>
+                                        </Link>
+                                    </li>
+                                }
                             </ul>
                         </Col>
                     </Row>
--- src/components/wrappers/AppWrapper.tsx
+++ src/components/wrappers/AppWrapper.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { INavigationMenuFields } from "@src/types/generated/contentful";
-import NavigationMenu from "@components/layout/NavigationMenu";
-import menuData from '@utils/menu.preval';
-import { Container, Col, Row } from "react-bootstrap";
-import { FaEnvelope, FaFacebook } from 'react-icons/fa';
-import Link from "next/link";
+import siteData from '@utils/siteData.preval';
 import Header from "@components/layout/Header";
-import PageTitleSection from "@components/layout/PageTitleSection";
 import Footer from "@components/layout/Footer";
 import Script from "next/script";
 
 const AppLayout: React.FC = ({ children }) => {
-    const menu = menuData as INavigationMenuFields;
-    const menuItems = menu.menuItems!;
+    const data = siteData;
+    const menuItems = siteData.menuItems!;
 
     return (
         <>
@@ -20,7 +14,7 @@ const AppLayout: React.FC = ({ children }) => {
             <Script src="/js/script.js" />
             <Header menuItems={menuItems} />
             {children}
-            <Footer />
+            <Footer settings={data.settings} />
         </>
     );
 };
